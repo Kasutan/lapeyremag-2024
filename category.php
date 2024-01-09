b707ce1582e045bf6eeda20ba62eb9f78f330fb8
Kasutan <contact@kasutan.pro>
2024-01-09 18:57:11 +0100

    Compte et pagination des articles dans archive cat

--- category.php
+++ category.php
@@ -65,17 +65,36 @@ echo '<main class="site-main">';
 		if ( have_posts() ) :
 
 			echo '<div id="archive-filtrable">';
-			echo '<ul class="loop list">';
+				echo '<ul class="loop list">';
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-				get_template_part( 'partials/archive');
-			endwhile;
-	
-			echo '</ul>';
-			echo '<div class="pagination-wrap"><ul class="pagination"></ul><p>xx sur xxx</p></div>';
-			echo '</div>';
-	
+				/* Start the Loop */
+				$count=0;
+				while ( have_posts() ) : the_post();
+					$count++;
+					get_template_part( 'partials/archive');
+				endwhile;
+		
+				echo '</ul>';
+
+				echo '<div class="pagination-wrap">';
+					printf('<div class="count"><strong id="nb-display">6</strong> sur %s</div>',$count);
+					?><div class="pagination-list">
+						<a class="pagination-prev disabled nav" href="#"  title="Page précédente">
+						<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0.75 6.00492C0.75 5.8769 0.769817 5.74888 0.809451 5.63071C0.849085 5.51253 0.918445 5.40421 1.01753 5.30573L5.60518 0.756043C5.78354 0.578782 6.01143 0.5 6.27896 0.5C6.54649 0.5 6.7843 0.598478 6.96265 0.785586C7.14101 0.972695 7.24009 1.18935 7.24009 1.47493C7.24009 1.76052 7.15091 1.98702 6.96265 2.16428L3.09832 6.00492L6.99238 9.86526C7.17073 10.0524 7.25 10.2789 7.25 10.5448C7.25 10.8107 7.15091 11.0372 6.96265 11.2243C6.77439 11.4114 6.54649 11.5 6.26905 11.5C5.99162 11.5 5.75381 11.4114 5.57546 11.2243L1.01753 6.69427C0.918445 6.59579 0.849085 6.48747 0.809451 6.36929C0.769817 6.25112 0.75 6.13295 0.75 5.99508L0.75 6.00492Z" fill="#757575"/>
+						</svg>
+						</a>
+						<ul class="pagination"></ul>
+						<a class="pagination-next nav" href="#" title="Page suivante">
+							<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M15.25 11.9951C15.25 12.1231 15.2302 12.2511 15.1905 12.3693C15.1509 12.4875 15.0816 12.5958 14.9825 12.6943L10.3948 17.244C10.2165 17.4212 9.98857 17.5 9.72104 17.5C9.45351 17.5 9.2157 17.4015 9.03735 17.2144C8.85899 17.0273 8.75991 16.8107 8.75991 16.5251C8.75991 16.2395 8.84909 16.013 9.03735 15.8357L12.9017 11.9951L9.00762 8.13474C8.82927 7.94763 8.75 7.72113 8.75 7.45524C8.75 7.18935 8.84908 6.96285 9.03735 6.77574C9.22561 6.58863 9.45351 6.5 9.73094 6.5C10.0084 6.5 10.2462 6.58863 10.4245 6.77574L14.9825 11.3057C15.0816 11.4042 15.1509 11.5125 15.1905 11.6307C15.2302 11.7489 15.25 11.8671 15.25 12.0049L15.25 11.9951Z" fill="#757575"/>
+							</svg>
+						</a>
+					</div>
+				</div>
+
+			</div>
+		<?php
 		else :
 
 			get_template_part( 'partials/archive', 'none' );
--- js/pagination.js
+++ js/pagination.js
@@ -1,40 +1,89 @@
-(function($) {
+(function ($) {
 
-	$( document ).ready(function() {
+	$(document).ready(function () {
 		/*--------------------------------------------------------------
 		# Filtrer archives par catégorie
 		https://listjs.com/docs/
 		Si besoin de next et prev : https://codepen.io/gregh/pen/dJvNqb
 		--------------------------------------------------------------*/
+		var width = $(window).width();
+		var nbPosts = 6;
+		if (width >= 1024) {
+			nbPosts = 12;
+		}
 		var optionsListe = {
 			valueNames: ['categorie'],
-			page: 12, //TODO 6 en mobile
+			page: nbPosts,
 			pagination: {
-				outerWindow:1,
-				innerWindow:2,
+				outerWindow: 1,
+				innerWindow: 1,
 			},
-		
+
 		};
-	
+
 		var listePosts = new List('archive-filtrable', optionsListe);
+
+		//Mettre à jour le compte
+		updateCount();
 
 		//Au clic sur un élément de pagination, smooth scroll en haut de la liste
 		bindScroll(); // lier les écouteurs au premier affichage
 
 		//lier les écouteurs à chaque fois que la liste est mise à jour + attendre un peu pour que les liens de navigation soient reconstruits
-		listePosts.on('updated',function(list) {
+		listePosts.on('updated', function (list) {
 			
-			setTimeout(bindScroll,1000);
+
+			var isFirst = list.i == 1;
+			var isLast = list.i > list.matchingItems.length - list.page;
+
+			// make the Prev and Nex buttons disabled on first and last pages accordingly
+			$(".pagination-prev.disabled, .pagination-next.disabled").removeClass(
+				"disabled"
+			);
+			if (isFirst) {
+				$(".pagination-prev").addClass("disabled");
+			}
+			if (isLast) {
+				$(".pagination-next").addClass("disabled");
+			}
+
+			// hide pagination if there one or less pages to show
+			if (list.matchingItems.length <= nbPosts) {
+				$(".pagination-list").hide();
+			} else {
+				$(".pagination-list").show();
+			}
+
+			updateCount();
+			setTimeout(bindScroll, 1000);
 		})
 
+		//Actions sur les chevrons de navigation
+		$(".pagination-next").click(function () {
+			$(".pagination .active")
+				.next()
+				.trigger("click");
+		});
+		$(".pagination-prev").click(function () {
+			$(".pagination .active")
+				.prev()
+				.trigger("click");
+		});
+
+
 		function bindScroll() {
-			$('.pagination li').click(function(e) {
+			$('.pagination li').click(function (e) {
 				$("html, body").animate({
 					scrollTop: $('#archive-filtrable').offset().top - 150
-					}, 500);
+				}, 500);
+				//Déplacer focus
+				$('#archive-filtrable .vignette:first-child a').focus();
 			});
 		}
 
-	}); //fin document ready
-})( jQuery );
+		function updateCount() {
+			$('#nb-display').html(listePosts.visibleItems.length);
+		}
 
+	}); //fin document ready
+})(jQuery);

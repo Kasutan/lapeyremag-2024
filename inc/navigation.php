ab034cb96ed868bc1b8098e4024f844ac04cd44d
Kasutan <contact@kasutan.pro>
2024-01-18 23:06:15 +0100

    Liens navigation 1 et boutons statiques (à suivre)

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,20 +1,8 @@
 <?php
 /**
  * Navigation
- *
- * @package      EAStarter
- * @author       Bill Erickson
- * @since        1.0.0
- * @license      GPL-2.0+
 **/
-/**
-* Generate a class attribute and an AMP class attribute binding.
-*
-* @param string $default Default class value.
-* @param string $active  Value when the state enabled.
-* @param string $state   State variable to toggle based on.
-* @return string HTML attributes.
-*/
+
 
 /* walker for primary menu sub nav */
 class etcode_sublevel_walker extends Walker_Nav_Menu
@@ -35,11 +23,13 @@ class etcode_sublevel_walker extends Walker_Nav_Menu
 
 function kasutan_desktop_nav() {
 	echo '<nav id="site-navigation" class="nav-main" aria-label="menu principal">';
-		if( has_nav_menu( 'primary' ) ) {
-			wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'container_class' => 'nav-primary' ) );
-		}
-		
+		printf('<a href="#" id="ouvrir-menu-desktop"><span>Produits</span>%s</a>',kasutan_picto(array('icon'=>'chevron-bas')));
+		kasutan_affiche_navigation1();
+		echo '<div class="boutons">';
+			kasutan_affiche_boutons_header(true);
+		echo '</div>';
 	echo '</nav>';
+	
 }
 
 
@@ -50,32 +40,58 @@ function kasutan_mobile_nav() {
 		<?php echo kasutan_picto(array('icon'=>'close', 'class' => 'fermer-menu','size'=>'28'));?>
 	</button>
 	<div class="volet-navigation"  id="volet-navigation">
+		<nav class="nav-menu">
 		<?php
-		if( class_exists('etcode_sublevel_walker') ) {
-			wp_nav_menu( array(
-				'theme_location' => 'primary',
-				'menu_id'        => 'menu-mobile',
-				'walker' => new etcode_sublevel_walker,
-				'menu_class'=>'menu-mobile',
-			) );
-		} else {
-			wp_nav_menu( array(
-				'theme_location' => 'primary',
-				'menu_id'        => 'menu-mobile',
-				'menu_class'=>'menu-mobile',
-			) );
-		}
+		printf('<a href="#" id="ouvrir-menu-mobile"><span>Produits</span>%s</a>',kasutan_picto(array('icon'=>'chevron-droite')));
+		kasutan_affiche_navigation1();
+		echo '</nav>';
+		echo '<div class="boutons">';
+			kasutan_affiche_boutons_header();
+		echo '</div>';
 	echo '</div>'; //Fin volet navigation
 }
 
+function kasutan_affiche_navigation1() {
+	//Elements obtenus par API
+	$liens=get_option('lapeyre_headers_navigation1',false);
+	if(!empty($liens)) {
+		foreach($liens as $lien) {
+			$attr='';
 
-/**
- * Archive Paginated Navigation
- *
- */
-add_action( 'tha_content_while_after', 'kasutan_archive_paginated_navigation',20 );
-function kasutan_archive_paginated_navigation() {
-	if( ! is_singular() )
-	the_posts_pagination();
+			if($lien->target != "none") {
+				$attr='target="_blank" rel="noopener noreferrer"';
+			}
+
+			printf('<a href="%s" class="%s" %s>%s</a>',$lien->href,strtolower($lien->class),$attr,$lien->text);
+		}
+	}
 }
 
+
+function kasutan_affiche_boutons_header() {
+	if(!function_exists('get_field')) {
+		return;
+	}
+	$bouton1=get_field('lapeyre_header_bouton_1','option');
+	$bouton2=get_field('lapeyre_header_bouton_2','option');
+
+	if($bouton1) {
+		printf('<a href="%s" class="bouton ecoute" target="%s" rel="noopener noreferrer">%s<span>%s</span></a>',
+			esc_url($bouton1['url']),
+			esc_attr($bouton1['target']),
+			kasutan_picto(array('icon'=>'ecoute')),
+			wp_kses_post( $bouton1['title'] )
+		);
+	}
+
+	if($bouton2) {
+		printf('<a href="%s" class="bouton bleu" target="%s" rel="noopener noreferrer">%s<span>%s</span></a>',
+			esc_url($bouton2['url']),
+			esc_attr($bouton2['target']),
+			kasutan_picto(array('icon'=>'projet')),
+			wp_kses_post( $bouton2['title'] )
+		);
+	}
+
+	
+}
--- js/navigation.js
+++ js/navigation.js
@@ -21,11 +21,6 @@
 		return;
 	}
 
-	menu = volet.getElementsByTagName( 'ul' )[0];
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
-		menu.className += ' nav-menu';
-	}
-
 
 	volet.setAttribute( 'aria-expanded', 'false' );
 	button.setAttribute( 'aria-expanded', 'false' );
@@ -44,7 +39,7 @@
 	};
 
 	// Get all the link elements within the menu.
-	links    = menu.getElementsByTagName( 'a' );
+	links    = volet.getElementsByTagName( 'a' );
 
 	// Each time a menu link is focused or blurred, toggle focus.
 	for ( i = 0, len = links.length; i < len; i++ ) {
